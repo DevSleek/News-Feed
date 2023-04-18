a3675f5d5ef8c9276bf4a23df5b834f58e2ea102
SukhrobTuraev <104163417+SukhrobTuraev@users.noreply.github.com>
2023-04-18 09:52:05 +0500

    yangiliklar sayti

---
+++ news_app/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import (
+    news_list,
+    news_detail,
+    HomePageView,
+    ContactPageView,
+    errorPageView,
+    aboutPageView,
+    singlePageView,
+    LocalNewsView,
+    ForeignNewsView,
+    TechnologyNewsView,
+    SportNewsView
+)
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home_page'),
+    path('news/', news_list, name='all_news_list'),
+    path('news/<slug:news>/', news_detail, name='news_detail_page'),
+    path('contact-us/', ContactPageView.as_view(), name='contact_page'),
+    path('error/', errorPageView, name='error_page'),
+    path('about/', aboutPageView, name='about_page'),
+    path('single/', singlePageView, name='single_page'),
+    path('local/', LocalNewsView.as_view(), name='local_news_page'),
+    path('foreign/', ForeignNewsView.as_view(), name='foreign_news_page'),
+    path('technology/', TechnologyNewsView.as_view(), name='technology_news_page'),
+    path('sport/', SportNewsView.as_view(), name='sport_news_page')
+]
+
